81905a52a723850030a42f7edcd8e8fe68f45d20
ExtremeCode <extremecode716@gmail.com>
2021-07-16 00:29:08 +0900

    [Add] TimeTrace Aop @Aspect @Around

---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,24 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+
+    @Around("execution(* hello.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint jointPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + jointPoint.toString());
+        try {
+            return jointPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + jointPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
